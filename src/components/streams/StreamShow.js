7123cf95c1c657fc821fea63fae0daddf4f76e9e
Evan Yip <etyip@ucsb.edu>
2019-09-12 11:41:58 -0700

    working on streamShow

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 
 class StreamShow extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.videoRef = React.createRef();
+    }
+
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id)
     }
+
     render() {
         if (!this.props.stream) {
             return <div>Loading...</div>
@@ -15,6 +23,7 @@ class StreamShow extends React.Component {
         const { title, description } = this.props.stream;
         return (
             <div>
+                <video ref={this.videoRef} style={{ width: '100%' }} controls />
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
